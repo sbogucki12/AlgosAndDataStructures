87de648e54c85036fe60bc837b5d6d2b089b9b37
Steve B <sbogucki@mail.usf.edu>
2021-05-24 21:00:47 -0700

    added binary tree in-order traversal

--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@ namespace AlgosAndDataStructures
         static void Main(string[] args)
         {
             TreeInOrder treeInOrder = new TreeInOrder();
-           
+
             IList<int> inOrderTraversal = treeInOrder.InorderTraversal(new TreeNode(val: 1, left: new TreeNode(val: 5, new TreeNode(val: 5)), right: new TreeNode(val: 3, null, new TreeNode(val: 6))));
 
-            for(var i = 0; i < inOrderTraversal.Count; i++)
+            for (var i = 0; i < inOrderTraversal.Count; i++)
             {
                 //should be 5, 5, 1, 3, 6
                 Console.Write("{0}, ", inOrderTraversal[i]);
--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
 ﻿This is a repository to store solutions to various coding challenges, as I complete them. 
 
+## May 24, 2021
+
+<p>Binary Tree, In-Order Traversal:</p>
+
+<img src="https://raw.githubusercontent.com/sbogucki12/AlgosAndDataStructures/main/readmeImages/binarDebug.jpg" />
+
+
 ## May 22, 2021
 
 <p>Binary Tree, Pre-Order Traversal:</p>
