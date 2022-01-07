9d7b0ac9951bc47490ba8af21126ab35171998cf
Steve B <sbogucki@mail.usf.edu>
2022-01-06 18:33:51 -0800

    completed hackerrank divisible sum pairs

--- challenges/Challenge.cs
+++ challenges/Challenge.cs
@@ -15,13 +15,13 @@ namespace AlgosAndDataStructures.challenges
         {
             int[] ans = new int[nums.Length];
 
-            for(int i = 0; i < nums.Length; i ++)
+            for (int i = 0; i < nums.Length; i++)
             {
                 ans[i] = nums[nums[i]];
             }
 
             return ans;
-        }        
+        }
     };
 
     public class Solution
@@ -71,13 +71,13 @@ namespace AlgosAndDataStructures.challenges
     {
         public static string kangaroo(int x1, int v1, int x2, int v2)
         {
-            
-            if(v2 > v1)
+
+            if (v2 > v1)
             {
                 return "NO";
             }
 
-            if(x2 - x1 % v2 - v1 == 0)
+            if (x2 - x1 % v2 - v1 == 0)
             {
                 return "YES";
             }
@@ -87,28 +87,56 @@ namespace AlgosAndDataStructures.challenges
 
         public static int birthday(List<int> s, int d, int m)
         {
-            //loop through indexes
-            //add m indexes at each iteration
-            //check if d - sum == 0
-            //if so increment  count
+            // a sub-list of 's', of length 'm' must add up to 'd'
 
             int count = 0;
-            int sum = 0; 
 
-            for(int i = 0; i < s.Count - m; i++)
+            // use GetRange with an incrementing initial index 'j'
+            // of length 'm' to get sub-lists.
+            // Sum up all indexes in each sub-list and check if 
+            // each sublist minus 'd' equals 0. 
+            // If so, increment count.  
+            // Count is the total number of sub-Lists of length 'm'
+            // from list 's' that equal 'd'
+
+            for (int j = 0; j < s.Count - (m - 1); j++)
             {
-                for(int j = 0; j <= m; j++)
+                var newS = s.GetRange(j, m);
+
+                int sum = 0;
+                for (int i = 0; i < m; i++)
                 {
-                    sum += s[j];
+                    sum += newS[i];
                 }
 
-                if(d - sum == 0)
+                if (sum - d == 0)
                 {
                     count++;
                 }
             }
 
-            return count; 
+            return count;
+        }
+
+        public static int divisibleSumPairs(int n, int k, List<int> ar)
+        {
+            ar.Sort();
+            int result = 0;
+            // loop through the array, adding index "i" to
+            // index "j" next to it. 
+            // If "k" divided by that sum has no remainder
+            // increment the result. 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if ((ar[i] + ar[j]) % k == 0)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
         }
     };
 }
